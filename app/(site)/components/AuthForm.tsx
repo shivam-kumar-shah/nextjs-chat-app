1462f5b19883c7a20d043c6e3a18f58d3302faab
shivam-2701 <shivam.shah2701@gmail.com>
2023-08-24 19:20:28 +0530

    Folder created

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  return <div>Auth Form!</div>;
+};
+
+export default AuthForm;
